7a27d316de8693835582683510331f08f5afd29c
gsr142 <gsr142@gmail.com>
2023-07-11 15:42:14 -0700

    add event listener for user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
+    
+    var geoCode = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=d9603dd48b308569c2d9d4504c34811f";
+
+    var weatherCall = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=d9603dd48b308569c2d9d4504c34811f";
+
+    
 
 
 
-
-
-
-
+   $(".submit").click(function(){
+    var userInput = $('.userInput').val();
+    console.log(userInput)
+   });
+    
+    
 });
