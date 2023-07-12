6fbf32383e8dbad99b1bdaad06822996b600b941
gsr142 <gsr142@gmail.com>
2023-07-11 22:49:11 -0700

    something wrong with userInput. too late to work on right now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
+    var userInput = $('.userInput').val();
     
-    var geoCode = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=d9603dd48b308569c2d9d4504c34811f";
+    
+    
+    var geoCode = "http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=10&appid=d9603dd48b308569c2d9d4504c34811f";
 
     var weatherCall = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=d9603dd48b308569c2d9d4504c34811f";
 
@@ -9,9 +12,22 @@ $(document).ready(function() {
 
 
    $(".submit").click(function(){
-    var userInput = $('.userInput').val();
-    console.log(userInput)
+        getCity()
    });
+
+   function getCity(){
+    console.log(userInput)
+    console.log(geoCode)
+    fetch(geoCode)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data)
+        })
+   }
+
+
     
     
 });
